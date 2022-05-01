9166f2f6d8ae473842cb7909bb41ed9d9e4e585e
shahriarlimon <shahriarwebdev@gmail.com>
2022-05-01 23:00:35 +0600

    added login and registration section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Authentication/Login/Login';
+import Register from './Components/Authentication/Register/Register';
 import Home from './Components/Pages/Home/Home';
 import Footer from './Components/Shared/Footer/Footer';
 import Header from './Components/Shared/Header/Header';
@@ -13,6 +14,7 @@ function App() {
      <Routes>
        <Route path='/' element={<Home/>}/>
        <Route path='/login' element={<Login/>}/>
+       <Route path='/register' element = {<Register/>}/>
      </Routes>
     
      <Footer/>
---
+++ src/Components/Authentication/Register/Register.js
@@ -0,0 +1,169 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({ emailError: "", passwordError: "" });
+  const handleInputEmail = (e) => {
+      const emailRegex = /\S+@\S+\.\S+/;
+      const validEmail = emailRegex.test(e.target.value);
+      if(validEmail){
+          setUserInfo({...userInfo,email:e.target.value});
+          setErrors({...errors,emailError:''})
+      }else{
+          setUserInfo({...userInfo, email:''});
+          setErrors({...errors,emailError:'Invalid Email'})
+      }
+  };
+  const handleInputPassword = (e) => {
+      const passwordRegex = /.{6,}/;
+      const validPassword = passwordRegex.test(e.target.value);
+      if(validPassword){
+          setUserInfo({...userInfo, password:e.target.value});
+          setErrors({...errors,passwordError:''});
+      }else{
+          setUserInfo({...userInfo,password:''});
+          setErrors({...errors,passwordError:"Minimum 6 characters!"})
+      }
+  };
+  const handleInputConfirmPassword = (e) => {
+      if(e.target.value === userInfo.password){
+          setUserInfo({...userInfo,confirmPassword:e.target.value});
+          setErrors({...errors,passwordError:''})
+      }
+      else{
+          setUserInfo({...userInfo, confirmPassword:''});
+          setErrors({...errors, passwordError: "Password didn't match!"});
+      }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className=" flex items-center justify-center my-16">
+      <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm w-1/2">
+        <form onSubmit={handleOnSubmit}>
+          <div class="form-group mb-6">
+            <label class="form-label inline-block mb-2 text-gray-700">
+              Email address
+            </label>
+            <input
+              required
+              onBlur={handleInputEmail}
+              type="email"
+              class="form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleInputEmail2"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="form-group mb-6">
+            <label class="form-label inline-block mb-2 text-gray-700">
+              Password
+            </label>
+            <input
+              required
+              onBlur={handleInputPassword}
+              type="password"
+              class="form-control block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleInputPassword2"
+              placeholder="Password"
+            />
+          </div>
+          <div class="form-group mb-6">
+            <label class="form-label inline-block mb-2 text-gray-700">
+              Confirm Password
+            </label>
+            <input
+              required
+              onBlur={handleInputConfirmPassword}
+              type="password"
+              class="form-control block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleInputPassword2"
+              placeholder=" Confirm Password"
+            />
+          </div>
+          <button
+            type="submit"
+            class="
+        w-full
+        px-6
+        py-2.5
+        bg-blue-600
+        text-white
+        font-medium
+        text-xs
+        leading-tight
+        uppercase
+        rounded
+        shadow-md
+        hover:bg-blue-700 hover:shadow-lg
+        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-blue-800 active:shadow-lg
+        transition
+        duration-150
+        ease-in-out"
+          >
+            Register
+          </button>
+          <p class="text-gray-800 mt-6 text-center text-sm">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              class="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out"
+            >
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
