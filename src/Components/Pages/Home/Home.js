305c55275f8258e4994736d16c6adfdf136a7b72
shahriarlimon <shahriarwebdev@gmail.com>
2022-05-07 15:18:05 +0600

    added gallery section

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from './Carousel/Carousel';
 import Cars from './Carousel/Cars/Cars';
+import Gallery from './Gallery/Gallery';
 import Testimonials from './Testimonials/Testimonials';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Carousel/>
             <Cars/>
             <Testimonials/>
+            <Gallery/>
         </div>
     );
 };
